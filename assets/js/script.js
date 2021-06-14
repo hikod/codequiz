ebed0a8456bb7b4c7e79ee42cb16d89b1b22c5d6
Hikmet Dogrul <hikmetdogrul00@gmail.com>
2021-06-14 00:42:01 -0400

    adding countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,32 @@
 var startQuizBtn = document.getElementById("startQuizBtn");
+var timerEl = document.getElementById('countdown');
 
-function clearBox(elementID)
-{
+// Timer that counts down from 5
+function countdown() {
+    var timeLeft = 75;
+
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+        // As long as the `timeLeft` is greater than 1
+        if (timeLeft > 0) {
+            // Set the `textContent` of `timerEl` to show the remaining seconds
+            timerEl.textContent = "Time: " + timeLeft;
+            // Decrement `timeLeft` by 1
+            timeLeft--;
+        } else {
+            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+            timerEl.textContent = '';
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+
+function clearBox(elementID) {
     document.getElementById(elementID).innerHTML = "";
 }
 
- startQuizBtn.addEventListener("click", function(){
+startQuizBtn.addEventListener("click", function () {
     clearBox("firstRow");
- });
+    countdown();
+});
